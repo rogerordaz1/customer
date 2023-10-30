a486f57ecedd7d6e8abc1ebc46c6197d84c15d39
Roger Ordaz <rogerordaz98@gmail.com>
2023-10-30 15:53:34 -0400

    stash

--- lib/core/app_pages.dart
+++ lib/core/app_pages.dart
@@ -4,6 +4,10 @@ import 'package:app/modules/auth/register/routes/register_routes.dart';
 import 'package:app/modules/home/routes/home_routes.dart';
 import 'package:app/modules/layout/routes/layout_routes.dart';
 import 'package:app/modules/maps/maps_routes.dart';
+import 'package:app/modules/my_reservations/my_reservation.dart';
+import 'package:app/modules/notifications/notification_routes.dart';
+import 'package:app/modules/search/routes/search_routes.dart';
+import 'package:app/modules/settings/settings_routes.dart';
 
 class AppPages {
   AppPages._();
@@ -17,5 +21,9 @@ class AppPages {
     ...LayoutRoutes.routes,
     ...HomeRoutes.routes,
     ...MapsRoutes.routes,
+    ...SettingsRoutes.routes,
+    ...NotificationRoutes.routes,
+    ...MyReservationRoutes.routes,
+    ...SearchRoutes.routes
   ];
 }
